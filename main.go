64c3df1fbdd7ce4bb1f357ce9139a306fe4e5303
Corey Massey <coreymassey@gmail.com>
2017-03-08 16:51:24 -0700

    seperateIntoFunctions

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ type Bottle struct {
 }
 
 func main() {
+	differentCreations()
+	dereferencePointer()
+	passingPointer()
+}
+
+func differentCreations() {
 	// use new to instantiate a new bottle with default values and get a pointer back
 	bottle := new(Bottle)
 	log.Printf("capcolor: %v", bottle.CapColor)
@@ -19,7 +25,9 @@ func main() {
 	bottle2 := &Bottle{}
 	log.Printf("2 capcolor: %v", bottle2.CapColor)
 	log.Printf("2 height: %v", bottle2.BottleHeight)
+}
 
+func dereferencePointer() {
 	// pass a dereferenced pointer to get a copy, not the object
 	bottle3 := &Bottle{
 		CapColor:     "red",
@@ -29,7 +37,9 @@ func main() {
 	// will be the same as when we instantiated since we dereference before we pass in
 	log.Printf("3 capcolor: %v", bottle3.CapColor)
 	log.Printf("3 height: %v", bottle3.BottleHeight)
+}
 
+func passingPointer() {
 	// pass a pointer to change the original object
 	bottle4 := &Bottle{
 		CapColor:     "yellow",
@@ -39,7 +49,6 @@ func main() {
 	// will be changed to blue and 11 because the reference data was changed
 	log.Printf("4 capcolor: %v", bottle4.CapColor)
 	log.Printf("4 height: %v", bottle4.BottleHeight)
-
 }
 
 func testPassPointer(b *Bottle) {
